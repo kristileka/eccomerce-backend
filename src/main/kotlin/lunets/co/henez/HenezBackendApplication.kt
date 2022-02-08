29b38a9fa528e8da634223e4c85ea0bbd2baa3e6
Kristi Leka <m.kristileka@gmail.com>
2022-02-08 20:32:40 +0100

    first commit

---
+++ src/main/kotlin/lunets/co/henez/HenezBackendApplication.kt
@@ -0,0 +1,11 @@
+package lunets.co.henez
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HenezBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<HenezBackendApplication>(*args)
+}
---
+++ src/test/kotlin/lunets/co/henez/HenezBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package lunets.co.henez
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HenezBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
