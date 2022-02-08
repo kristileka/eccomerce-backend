bb36a2ee43720086b9630c82820b57fc0f2727fd
Kristi Leka <m.kristileka@gmail.com>
2022-02-08 20:46:56 +0100

    heroku config

--- src/main/kotlin/lunets/co/henez/HenezBackendApplication.kt
+++ src/main/kotlin/lunets/co/henez/HenezBackendApplication.kt
@@ -2,9 +2,19 @@ package lunets.co.henez
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+
 
 @SpringBootApplication
 class HenezBackendApplication
+
+@RequestMapping("/")
+@ResponseBody
+fun home(): String? {
+    return "Hello World!"
+}
+
 
 fun main(args: Array<String>) {
     runApplication<HenezBackendApplication>(*args)
