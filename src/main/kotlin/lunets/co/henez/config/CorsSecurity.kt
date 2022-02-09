56874f93898b83ca2317d2f78eddf78c6c635450
Kristi Leka <m.kristileka@gmail.com>
2022-02-10 00:19:01 +0100

    asd

--- src/main/kotlin/lunets/co/henez/config/CorsSecurity.kt
+++ src/main/kotlin/lunets/co/henez/config/CorsSecurity.kt
@@ -1,27 +1,13 @@
 package lunets.co.henez.config
 
-import org.springframework.context.annotation.Bean
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.CorsConfigurationSource
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @EnableWebSecurity
 class WebSecurityConfig : WebSecurityConfigurerAdapter() {
 
     override fun configure(http: HttpSecurity) {
-        http.cors()
-    }
-
-    @Bean
-    fun corsConfigurationSource(): CorsConfigurationSource {
-        val configuration = CorsConfiguration();
-        configuration.allowedOrigins = listOf("*")
-        configuration.allowedMethods = listOf("GET", "POST", "PATCH")
-        val source = UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration)
-        return source;
+        http.cors().and().csrf().disable()
     }
 }
