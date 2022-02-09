219c3b0f9d58bffbbcb1fee32b962d95d3c10d8d
Kristi Leka <m.kristileka@gmail.com>
2022-02-10 00:34:14 +0100

    asdasd

--- src/main/kotlin/lunets/co/henez/config/CorsSecurity.kt
+++ src/main/kotlin/lunets/co/henez/config/CorsSecurity.kt
@@ -3,11 +3,27 @@ package lunets.co.henez.config
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.web.cors.CorsConfiguration
+import java.util.List
+import javax.servlet.http.HttpServletRequest
 
 @EnableWebSecurity
 class WebSecurityConfig : WebSecurityConfigurerAdapter() {
 
+
+    @Throws(Exception::class)
     override fun configure(http: HttpSecurity) {
-        http.cors().and().csrf().disable()
+        val corsConfiguration = CorsConfiguration()
+        corsConfiguration.allowedHeaders = listOf("Authorization", "Cache-Control", "Content-Type")
+        corsConfiguration.allowedOrigins = listOf("*")
+        corsConfiguration.allowedMethods =
+            listOf("GET", "POST", "PUT", "DELETE", "PUT", "OPTIONS", "PATCH", "DELETE")
+        corsConfiguration.allowCredentials = true
+        corsConfiguration.exposedHeaders = listOf("Authorization")
+
+        // You can customize the following part based on your project, it's only a sample
+        http.authorizeRequests().antMatchers("/**").permitAll().anyRequest()
+            .authenticated().and().csrf().disable().cors()
+            .configurationSource { corsConfiguration }
     }
 }
